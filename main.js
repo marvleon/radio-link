0901dfa51e9af94257b379c9cfbb9d68fac45a7b
marvleon <marvleon@pdx.edu>
2024-05-17 13:10:28 -0700

    initial commit from local repo

---
+++ main.js
@@ -0,0 +1,101 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+const {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  StreamType,
+  VoiceConnectionStatus,
+} = require("@discordjs/voice");
+const child_process = require("child_process");
+const ffmpeg = require("ffmpeg-static");
+
+const token = process.env.DISCORD_TOKEN;
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+let voiceConnection;
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("messageCreate", async (message) => {
+  if (!message.guild) return;
+
+  if (message.content === "/join") {
+    if (message.member.voice.channel) {
+      if (
+        !voiceConnection ||
+        voiceConnection.joinConfig.channelId !== message.member.voice.channel.id
+      ) {
+        voiceConnection = joinVoiceChannel({
+          channelId: message.member.voice.channel.id,
+          guildId: message.guild.id,
+          adapterCreator: message.guild.voiceAdapterCreator,
+          selfDeaf: false,
+        });
+
+        voiceConnection.on(VoiceConnectionStatus.Ready, () => {
+          console.log("The bot has connected to the channel!");
+          message.reply("Joined the voice channel!");
+        });
+      } else {
+        message.reply("Already connected to this voice channel!");
+      }
+    } else {
+      message.reply("You need to join a voice channel first!");
+    }
+  } else if (message.content === "/play kmhd") {
+    if (!voiceConnection) {
+      message.reply("I need to be in a voice channel first. Use `/join`.");
+      return;
+    }
+
+    const m3u8Url =
+      "https://ais-sa3.cdnstream1.com/2442_128.aac/playlist.m3u8?aw_0_1st.playerid=esPlayer&aw_0_1st.skey=1715963358";
+
+    const ffmpegProcess = child_process.spawn(
+      ffmpeg,
+      [
+        "-i",
+        m3u8Url,
+        "-acodec",
+        "pcm_s16le", // Output audio as raw PCM
+        "-ar",
+        "48000", // Sample rate
+        "-ac",
+        "2", // Stereo
+        "-f",
+        "s16le", // Output format for PCM
+        "pipe:1", // Output to a pipe
+      ],
+      { stdio: ["ignore", "pipe", "pipe"] }
+    );
+
+    ffmpegProcess.stderr.on("data", (data) => {
+      console.log(`FFmpeg: ${data.toString()}`);
+    });
+
+    const resource = createAudioResource(ffmpegProcess.stdout, {
+      inputType: StreamType.Raw,
+      inlineVolume: true,
+    });
+    resource.volume.setVolume(1);
+    const player = createAudioPlayer();
+
+    player.play(resource);
+    voiceConnection.subscribe(player);
+
+    message.reply("Playing now!");
+  }
+});
+
+client.login(token);
